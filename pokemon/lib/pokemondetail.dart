ef685e328729acd68dfaad22df9866283ae59c9b
Furkankyl <furkan.kayali@bil.omu.edu.tr>
2019-09-19 15:43:49 +0300

    poke detail added

---
+++ pokemon/lib/pokemondetail.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon/pokemon.dart';
+
+class PokeDetail extends StatelessWidget {
+  final Pokemon pokemon;
+
+  PokeDetail(this.pokemon);
+
+  bodyWidget(BuildContext context) => Stack(
+        children: <Widget>[
+          Positioned(
+            height: MediaQuery.of(context).size.height / 1.5,
+            width: MediaQuery.of(context).size.width - 20,
+            left: 10,
+            top: MediaQuery.of(context).size.height * 0.1,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SizedBox(
+                    height: 70.0,
+                  ),
+                  Text(
+                    pokemon.name,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text("Height: ${pokemon.height}"),
+                  Text("Weight: ${pokemon.weight}"),
+                  Text(
+                    "Types",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: pokemon.type
+                        .map((t) => FilterChip(
+                              backgroundColor: Colors.amber,
+                              label: Text(t),
+                              onSelected: (b) {},
+                            ))
+                        .toList(),
+                  ),
+                  Text(
+                    "Weakness",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: pokemon.weaknesses
+                        .map((t) => FilterChip(
+                              backgroundColor: Colors.red,
+                              label: Text(
+                                t,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              onSelected: (b) {},
+                            ))
+                        .toList(),
+                  ),
+                  Text(
+                    "Next Evolution",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  nextEvolution(),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Hero(
+              tag: pokemon.img,
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.fitWidth,
+                        image: NetworkImage(pokemon.img))),
+              ),
+            ),
+          )
+        ],
+      );
+
+  nextEvolution() => pokemon.nextEvolution != null
+      ? Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: pokemon.nextEvolution
+              .map((t) => FilterChip(
+                    backgroundColor: Colors.green,
+                    label: Text(
+                      t.name,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onSelected: (b) {},
+                  ))
+              .toList(),
+        )
+      : Text("Already last evolution");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.cyan,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.cyan,
+        title: Text(pokemon.name),
+      ),
+      body: bodyWidget(context),
+    );
+  }
+}
